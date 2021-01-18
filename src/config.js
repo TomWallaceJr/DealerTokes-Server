915bdd68a185caeacc22ebf9eca22813593f5010
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-18 12:05:10 -0500

    change

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
-    || 'postgresql://postgres@localhost/spaced_repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-}
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres@localhost/spaced_repetition",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
+};
--- src/server.js
+++ src/server.js
@@ -1,22 +1,34 @@
-require('dotenv').config()
-
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DB_URL } = require('./config')
-// const { CLIENT_ORIGIN } = require('./config');
-// const cors = require('cors');
-
+require("dotenv").config();
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
 const db = knex({
-  client: 'pg',
-  connection: DB_URL,
-})
-
-app.set('db', db)
-
-// app.use(cors({
-//   origin: CLIENT_ORIGIN
-// }))
-
+  client: "pg",
+  connection: DATABASE_URL,
+});
+app.set("db", db);
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
