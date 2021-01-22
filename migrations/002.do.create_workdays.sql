45b05fb732083cb036b2a206bd81b7d27c13eeb3
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-21 21:43:41 -0500

    seed files written

--- migrations/002.do.create_workdays.sql
+++ migrations/002.do.create_workdays.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "workdays" (
   "id" SERIAL PRIMARY KEY,
   "hours" FLOAT(2) NOT NULL,
-  "downs" INT DEFAULT 0 NOT NULL,
-  "tokes" INT DEFAULT 0 NOT NULL,
-  "date" TIMESTAMPZ NOT NULL,
+  "downs" TINYINT DEFAULT 0 NOT NULL,
+  "tokes" SMALLINT DEFAULT 0 NOT NULL,
+  "date" DATE NOT NULL,
   "notes" TEXT,
   "user_id" INTEGER REFERENCES "user"(id)
     ON DELETE CASCADE NOT NULL
--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,43 +1,37 @@
 BEGIN;
 
 TRUNCATE
-  "word",
-  "language",
+  "workdays",
   "user";
 
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
-    'admin',
-    'Dunder Mifflin Admin',
+    'JD123',
+    'John Doe',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
-INSERT INTO "language" ("id", "name", "user_id")
+INSERT INTO "workdays" ("id", "hours", "downs", "tokes", "date", "notes", "user_id")
 VALUES
-  (1, 'Spanish', 1);
-
-INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
-VALUES
-  (1, 1, 'hola', 'hello', 2),
-  (2, 1, 'adios', 'goodbye', 3),
-  (3, 1, 'por favor', 'please', 4),
-  (4, 1, 'gracias', 'thank you', 5),
-  (5, 1, 'lo siento', 'sorry', 6),
-  (6, 1, 'si', 'yes', 7),
-  (7, 1, 'perro', 'dog', 8),
-  (8, 1, 'gato', 'cat', 9),
-  (9, 1, 'computadora', 'computer', 10),
-  (10, 1, 'tienda', 'store', null);
-
-UPDATE "language" SET head = 1 WHERE id = 1;
-
--- because we explicitly set the id fields
--- update the sequencer for future automatic id setting
-SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
-SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
-SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+  (1, 8, 14, 321, 2020-11-01, "full shift 4 breaks", 1),
+  (2, 2, 14, 60, 2020-11-04, "signed EO", 1),
+  (3, 4, 14, 152, 2020-11-05, "tiger king", 1),
+  (4, 4.5, 14, 150, 2020-11-09, "aliens attacked", 1),
+  (5, 7, 14, 320, 2020-11-11, "full shift", 1),
+  (6, 9, 14, 444, 2020-11-14, "it was cold", 1),
+  (7, 2, 14, 41, 2020-11-15, "bomb scare", 1),
+  (8, 1, 14, 21, 2020-11-18, "EO", 1),
+  (10, 5, 14, 64, 2020-11-24, "meh", 1),
+  (11, 5, 14, 101, 2020-12-01, "big tipper", 1),
+  (12, 8, 14, 311, 2020-12-02, "full shift", 1),
+  (13, 8, 14, 321, 2020-12-05, "full shift", 1),
+  (14, 1, 14, 11, 2020-12-07, "EO", 1),
+  (15, 1.5, 14, 42, 2020-12-15, "signed EO", 1),
+  (16, 6, 14, 200, 2020-12-19, "it was cold", 1),
+  (17, 3.5, 14, 77, 2020-12-24, "santa coming", 1),
+  (18, 8, 14, 299, 2020-12-25, "merry christmas", 1);
 
 COMMIT;
