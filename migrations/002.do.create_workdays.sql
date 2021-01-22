3f4266d4e4d1bde4bd555a055fa7067f6a1c9f01
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-21 22:47:58 -0500

    db created and seeded

--- migrations/002.do.create_workdays.sql
+++ migrations/002.do.create_workdays.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "workdays" (
   "id" SERIAL PRIMARY KEY,
   "hours" FLOAT(2) NOT NULL,
-  "downs" TINYINT DEFAULT 0 NOT NULL,
-  "tokes" SMALLINT DEFAULT 0 NOT NULL,
+  "downs" INT DEFAULT 0 NOT NULL,
+  "tokes" INT DEFAULT 0 NOT NULL,
   "date" DATE NOT NULL,
   "notes" TEXT,
   "user_id" INTEGER REFERENCES "user"(id)
