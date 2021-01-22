45c0fe35f7eebceecaee6bf5c1b7027020ef8a8c
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-21 21:13:16 -0500

    deleted prev files

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./auth/auth-router');
-const languageRouter = require('./language/language-router');
+
 const userRouter = require('./user/user-router');
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use('/api/auth', authRouter);
-app.use('/api/language', languageRouter);
+
 app.use('/api/user', userRouter);
 // simple test to make sure responsive
 app.get('/', (req, res) => {
