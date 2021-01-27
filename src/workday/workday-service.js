ff1f2589b66be977cde4d337e1b77c5eb6abc775
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-27 16:24:30 -0500

    implement insertNewWorkday function in workdayService

--- src/workday/workday-service.js
+++ src/workday/workday-service.js
@@ -3,6 +3,16 @@ const WorkdayService = {
         return knex.select('*')
             .from('workdays')
     },
+
+    insertNewWorkday(knex, newEntry) {
+        return knex
+            .insert(newEntry)
+            .into('workday')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    }
 }
 
 module.exports = WorkdayService;
